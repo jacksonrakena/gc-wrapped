db7c8ad2fe1e6d40964b3060408794a1b91e03ea
jacksonrakena <jackson@rakena.co.nz>
2024-09-03 23:41:57 +1200

    Add documentation for deep property transforms

--- src/util/objects.ts
+++ src/util/objects.ts
@@ -1,17 +1,46 @@
 /**
- * Deep object transforms
+ * Represents a possibly deeply-nested JavaScript object.
+ *
+ * This type requires that all properties are either subtrees (compliant with the same type),
+ * or leaf nodes (which are required to be of type TLeaf).
  */
-
 export type DeepTree<TLeaf> = {
   [x: string]: TLeaf | DeepTree<TLeaf>;
 };
 
+/**
+ * Increments the property located in `obj` by following the access path indicated by `accessors`.
+ *
+ * If the property, or any object above it in the access path, does not exist, it will be created and
+ * initialised to `1` when this function is called.
+ *
+ * @example
+ * ```js
+ * const myObj = {};
+ * deepIncrement(myObj, 'this', 'is', 'deep');
+ * // myObj => { this: { is: { deep: 1 } } }
+ * ```
+ */
 export const deepIncrement = (
   obj: DeepTree<number>,
   ...accessors: string[]
 ) => {
   deepAdd(obj, 1, ...accessors);
 };
+
+/**
+ * Adds `incrementBy` to the property located in `obj` by following the access path indicated by `accessors`.
+ *
+ * If the property, or any object above it in the access path, does not exist, it will be created and
+ * initialised to `0 + incrementBy` when this function is called.
+ *
+ * @example
+ * ```js
+ * const myObj = {};
+ * deepAdd(myObj, 25, 'this', 'is', 'deep');
+ * // myObj => { this: { is: { deep: 25 } } }
+ * ```
+ */
 export const deepAdd = (
   obj: DeepTree<number>,
   incrementBy: number,
@@ -22,11 +51,37 @@ export const deepAdd = (
   );
 };
 
+/**
+ * Returns the value of the property located in `obj` by following the access path indicated by `accessors`.
+ * @example
+ * ```js
+ * const myObj = { this: { is: { deep: 'hello' } } };
+ * getDeepProperty(myObj, 'this', 'is', 'deep')
+ * // => 'hello'
+ * ```
+ */
 export const getDeepProperty = <TLeaf>(
   obj: DeepTree<TLeaf>,
   ...accessors: string[]
 ) => getDeepPropertyAccessor(obj, ...accessors).read();
 
+/**
+ * Transforms the value of the property located in `obj` by following the access path indicated by `accessors`,
+ * by applying the result of `transformationFunction` when executed.
+ *
+ * The transformation function is provided with the
+ * original value of the property (if it exists), or `undefined`, if the property has not previously been defined.
+ *
+ * If the property, or any object above it in the access path, does not exist, it will be created upon first write.
+ *
+ * This function returns the new value of the property.
+ * @example
+ * ```js
+ * const myObj = { this: { is: { deep: 'hello' } } };
+ * transformDeepProperty(myObj, v => v + ' world!', 'this', 'is', 'deep')
+ * // => 'hello world!'
+ * ```
+ */
 export const transformDeepProperty = <TLeaf>(
   obj: DeepTree<TLeaf>,
   transformationFunction: (value: TLeaf | undefined) => TLeaf | undefined,
@@ -41,6 +96,23 @@ interface DeepPropertyAccessor<TLeaf> {
   ) => TLeaf | undefined;
 }
 
+/**
+ * Returns an accessor interface that allows the caller to read and write the value of a deep property of `obj`,
+ * that is found by following the path indicated by `accessors`.
+ *
+ * If the property, or any object above it in the access path, does not exist, it will be created upon first write.
+ *
+ * This function returns the new value of the property.
+ * @example
+ * ```js
+ * const myObj = { this: { is: { deep: 'hello' } } };
+ * const accessor = getDeepPropertyAccessor(myObj, 'this', 'is', 'deep');
+ * const value = accessor.read();
+ * // value => 'hello'
+ * const newValue = accessor.write(v => v + ' world!')
+ * // newValue (and also now, accessor.read()) => 'hello world!'
+ * ```
+ */
 const getDeepPropertyAccessor = <TLeaf>(
   obj: DeepTree<TLeaf>,
   ...accessors: string[]
